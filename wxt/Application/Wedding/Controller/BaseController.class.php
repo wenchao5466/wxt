335ec8c1d54a20859bf2ce8220c03ef8617ba567
宋文超 <songwenchao@ippjr.com>
2015-04-09 00:31:22 +0800

    submit

--- wxt/Application/Wedding/Controller/BaseController.class.php
+++ wxt/Application/Wedding/Controller/BaseController.class.php
@@ -9,24 +9,23 @@ class BaseController extends Controller {
 		$action = ACTION_NAME;
 		$controller = CONTROLLER_NAME;
 		//如果没有Session和Cookie
-		if(!cookie('user_message')  && !session('user_message')){
+	if(!cookie('user_message')  && !session('user_message')){
+// if(1){
 			$weObj = new Wechat($this->_options);
 			// 		$weObj->valid();//明文或兼容模式可以在接口验证通过后注释此句，但加密模式一定不能注释，否则会验证失败
 			$result = $weObj->getOauthAccessToken();
 			//如果是从微信返回的页面
 			if($result && $result['refresh_token']){
-				$a = $this->_getParam('a');
-				$c = $this->_getParam('c');
 				$weObj->getOauthRefreshToken($result['refresh_token']);
 				$user_message = $weObj->getOauthUserinfo($result['access_token'],$result['openid']);
 				session('user_message',$user_message);
-				var_dump(session('user_message'));
 				$user_item = M('user')->where(array('openid'=>$result['openid']))->find();
 				if(!$user_item){
 					$user_item['openid'] = $result['openid'];
 					$user_item['type'] = 'weixin';
 					$user_id = M('user')->save($user_item);
-			
+					$model = M();
+					$model->execute("insert into wxt_user set type='weixin',openid='".$result['openid']."',status=1 ");
 					cookie('user_id',$user_id);
 					session('user_id',$user_id);
 				}else{
@@ -34,7 +33,9 @@ class BaseController extends Controller {
 					cookie('user_id',$user_item['id']);
 				}
 				session('user_message',$user_message);
-				$this->redirect("/wxt/wedding/$c/$a");
+				cookie('user_message',$user_message);
+				$url = C('WEB_HOST')."/wxt/wedding";
+				header("location: $jump_url");
 			}else{
 				$this->jump($action,$controller);
 			}
@@ -50,9 +51,9 @@ class BaseController extends Controller {
 			return '';
 		}
 	}
-	public function jump($action,$controller){
+	public function jump(){
 		$weObj = new Wechat($this->_options);
-		$callback = C('WEB_HOST')."/wxt/wedding/?a=$action&c=$controller";
+		$callback = C('WEB_HOST')."/wxt/wedding/index/index";
 		$jump_url = $weObj->getOauthRedirect($callback,1);
 		header("location: $jump_url");
 	}
